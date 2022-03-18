d37c2314c313ff9e0a7e1cc69dd35f1030347b5e
SandeisonFernandes <97060385+sandeisonfernandes@users.noreply.github.com>
2022-03-18 07:56:06 -0300

    Activity Manager
    Modifique a aplicação anterior para que o Service consulte o ActivityManager e
    descubra qual o nome da Activity que está sendo exibida no momento.
    
    Exiba o resultado no mesmo Toast que exibe o número da Activity.

--- app/src/main/java/com/example/myapplication/MyIntentService.java
+++ app/src/main/java/com/example/myapplication/MyIntentService.java
@@ -1,9 +1,13 @@
 package com.example.myapplication;
 
+import android.app.ActivityManager;
 import android.app.IntentService;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.util.List;
 
 
 public class MyIntentService extends IntentService {
@@ -15,8 +19,23 @@ public class MyIntentService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         if (intent != null){
-            String name = intent.getStringExtra("TELA");
-            Toast.makeText(this, "Nome da Tela: "+name, Toast.LENGTH_LONG).show();
+            //String name = intent.getStringExtra("TELA");
+
+            ActivityManager activityManager =(ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+            if(activityManager != null) {
+                List<ActivityManager.AppTask> tasks = activityManager.getAppTasks();
+                // Maneira Nova de fazer a Activity Manager, retorna a activity que esta rodando no momento
+                String name;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    //Busca a activity esta no topo
+                    name = tasks.get(0).getTaskInfo().topActivity.getClassName();
+                }else{ // Maneira Antiga de verificar a Activity Manager ( Não se usa mais)
+                    name = activityManager.getRunningTasks(1).get(0).topActivity.getClassName();
+                }
+
+                Toast.makeText(this, "Nome da Tela: "+name, Toast.LENGTH_LONG).show();
+            }
+
         }
     }
 
